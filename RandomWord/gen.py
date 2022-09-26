938b9a4fe842306459745bbe23fea8b22b369c67
jason9071.eth <jason90712223@gmail.com>
2022-09-27 00:06:50 +0800

    fix : remove `strng.production` in python

--- RandomWord/gen.py
+++ RandomWord/gen.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def get_random_string(length):
-    letters = string.ascii_letters + string.punctuation + string.digits
+    letters = string.ascii_letters + string.digits
     return ''.join(random.choice(letters) for i in range(length))
 
 def main() :
